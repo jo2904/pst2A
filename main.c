2e9d74ea0572c5fb2c20a7584ae4d438a77f00cc
jo2904 <josephdegaullier@gmx.fr>
2022-04-26 16:05:04 +0200

    MAJ

--- main.c
+++ main.c
@@ -1,3 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <emmintrin.h>
+#include <unistd.h>
+
+//definition de la variation des donnés du bus => création de bruit
+__m128i un;
+__m128i zero;
+
+
+int signal(){
+    printf("signal");
+}
+
+int main(){
+    printf("lancement ...\n");
+
+    printf("initialisation ...\n");
+    zero = _mm_set_epi32(0, 0, 0, 0);
+    un = _mm_set_epi32(-1, -1, -1, -1);
+
+    
+    signal();
+}
